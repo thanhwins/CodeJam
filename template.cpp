eb533584284b97a52b83fc733d8fe1c41fc513fa
Nguyen Duc Thanh <37400717+thanhwins@users.noreply.github.com>
2023-05-21 21:02:13 +0700

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long MOD = 1000000007;
+#define int long long
+#define endl "\n"
+
+main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
